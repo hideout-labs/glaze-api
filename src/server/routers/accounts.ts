ad55b954295477c94d6d81f7f24d702e307a1b68
0xDistro <hey@0xDistro.com>
2024-10-09 01:25:05 +0100

    Adds check account method

--- src/server/routers/accounts.ts
+++ src/server/routers/accounts.ts
@@ -2,11 +2,40 @@ import { createActor, toPromise } from 'xstate';
 import { z } from 'zod';
 import { publicProcedure, router } from '~/server/trpc';
 import { registerAccountMachine, } from '~/server/machines/registerAccount.machine';
-import { getCircleAccount } from '~/server/utils/crypto';
+import { checkCircleAccount, getCircleAccount } from '~/server/utils/crypto';
 import { generateKey } from '~/server/utils/crypto';
 
 
 export const accountsRouter = router({
+    check: publicProcedure.meta({
+        openapi: {
+            method: 'GET', path: '/check', tags: ['accounts'],
+            summary: 'Check if an account is registered',
+        },
+        description: 'Check if an account is registered',
+    }).input(z.object(
+        {
+            id: z.string(),
+            appId: z.string().optional(),
+        }
+    )).output(
+        z.object({
+            isRegisteredInApp: z.boolean(),
+            isRegistered: z.boolean(),
+        })
+    ).query(async ({ input }) => {
+        // Check if ID is registered in App
+        const refId = generateKey(input.id);
+        const isRegisteredInApp = await checkCircleAccount({ refId, appId: input.appId });
+
+        // Check if ID is registered in Glaze
+        const isRegistered = await checkCircleAccount({ refId });
+
+        return {
+            isRegisteredInApp,
+            isRegistered,
+        }
+    }),
     register: publicProcedure.meta({
         openapi: {
             method: 'GET', path: '/register', tags: ['accounts'],
@@ -17,7 +46,7 @@ export const accountsRouter = router({
         {
             id: z.string(),
             pin: z.string(),
-            appId: z.string(),
+            appId: z.string().optional(),
         }
     )).output(
         z.object({
